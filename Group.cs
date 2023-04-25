8a03e79c69f2d5fb78cc8eefab36452a19e4b109
SamidAghayev <samidna@code.edu.az>
2023-04-26 01:35:35 +0400

    created student class

---
+++ Group.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApp5.Models;
+
+internal class Group
+{
+    public int No { get; set; }
+    public int Limit { get; set; }
+    private Student[] Students { get; set; } = new Student[0];
+
+    public Group(int limit)
+    {
+        Limit = limit;
+    }
+    public void AddStudent(Student student)
+    {
+        if (Students.Length < Limit)
+        {
+            Student[] List = new Student[Students.Length + 1];
+            for (int i = 0; i < Students.Length; i++)
+            {
+                List[i] = Students[i];
+            }
+            List[Students.Length] = student;
+            Students = List;
+        }
+        else
+        {
+            Console.WriteLine("Group is full.");
+        }
+    }
+    public string GetStudents()
+    {
+        string result = "Telebeler: ";
+        foreach (var student in Students)
+        {
+            result += $"{student.Name} {student.Surname}";
+        }
+        return result;
+    }
+}
+
+
+
+
---
+++ Student.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp5.Models;
+
+internal class Student
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+
+    public Student(string name, string surname)
+    {
+        Name = name;
+        Surname = surname;
+    
+    }
+    public void Fullname()
+    {
+        Console.WriteLine("Name:"+Name+"  "+"Surname:"+Surname);
+    }
+}
